91ec49edefe701211b2b6d7db56b813fb3974e47
Karter Livingston <kdlmuzixnerd14@gmail.com>
2022-09-08 15:24:51 -0700

    commit tree 7954bf899840146436b524fbcbbcbfb72a61b3f8

--- ruby/fibonacci.rb
+++ ruby/fibonacci.rb
@@ -1,23 +1,57 @@
 def fibonacci(num)
-  # type your code in here
+  # starting array
+  arr = [0, 1]
+
+  # stress cases
+  if num < 2
+    return arr[num]
+  end
+
+  # skip test cases
+  i = 2
+
+  # loop num times
+  while i<=num
+    # add 2 previous values together
+    val = arr[i-1] + arr[i-2]
+    # PUSH into array
+    arr << val
+    # add to counter to continue
+    i+=1
+  end
+  
+  # return value at num index
+  return arr.last
+
 end
 
-if __FILE__ == $PROGRAM_NAME
-  puts "Expecting: 0"
-  puts "=>", fibonacci(0)
+# tests
+p fibonacci(0)
+p fibonacci(1)
+p fibonacci(10)
+p fibonacci(14)
 
-  puts
+# if __FILE__ == $PROGRAM_NAME
+#   puts "Expecting: 0"
+#   puts "=>", fibonacci(0)
 
-  puts "Expecting: 1"
-  puts "=>", fibonacci(2)
+#   puts
 
-  puts
+#   puts "Expecting: 1"
+#   puts "=>", fibonacci(2)
 
-  puts "Expecting: 55"
-  puts "=>", fibonacci(10)
+#   puts
 
-  # Don't forget to add your own!
-end
+#   puts "Expecting: 55"
+#   puts "=>", fibonacci(10)
+
+#   # Don't forget to add your own!
+# end
 
 # Please add your pseudocode to this file
 # And a written explanation of your solution
+
+# make an array of numbers
+
+
+
